06c5fe20cae4ab8c5a5a0a0c756aa3107d96fd2c
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-05-30 10:07:22 +0530

    commit-2

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,9 @@
 
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+[
++src/main/java/com/app/db/HibernateUtil.java,5\c\5cb6dee7020ba289710d37f1c78a1dd959c2439d
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
---
+++ src/main/java/com/app/db/HibernateUtil.java
@@ -0,0 +1,44 @@
+package com.app.db;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class HibernateUtil {
+    private static SessionFactory SESSION_FACTORY;
+
+    static {
+        try {
+            SESSION_FACTORY = buildSessionFactory();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static SessionFactory buildSessionFactory() throws IOException {
+        Properties properties = new Properties();
+        properties.load(HibernateUtil.class.getClassLoader().getResourceAsStream("hibernate.properties"));
+        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(properties)
+                .build();
+
+        Metadata metadata = new MetadataSources(standardRegistry)
+
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                .build();
+
+        return metadata.getSessionFactoryBuilder().build();
+    }
+    public static Session getSession(){
+        return SESSION_FACTORY.openSession();
+    }
+}
