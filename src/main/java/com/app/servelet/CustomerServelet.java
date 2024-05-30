9d34a3422b6cb8c151f318c92270308ec75b7d97
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-05-30 17:49:16 +0530

    commit-3-ui-Servlet-1

--- src/main/java/com/app/servelet/CustomerServelet.java
+++ src/main/java/com/app/servelet/CustomerServelet.java
@@ -1,5 +1,9 @@
 package com.app.servelet;
 
+import com.app.controller.CustomerController;
+import com.app.entity.Customer;
+import lombok.extern.slf4j.Slf4j;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,10 +11,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 @WebServlet("/save")
 public class CustomerServelet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+        String salary = req.getParameter("salary");
+
+        Customer customer = new Customer(
+                Long.parseLong(id),name,address,Double.parseDouble(salary)
+        );
+        System.out.println("Customer : "+customer);
+        if(new CustomerController().saveCustomer(customer)){
+            System.out.println("Customer "+name+" saved...!");
+            resp.sendRedirect("/newCustomer.jsp");
+        }else{
+            System.out.println("Theres an Error........!");
+        }
     }
 }
